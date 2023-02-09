2dcdf0593604a058102a55b066205db0fd49ad19
Johanny <johanny_andrade@yahoo.com.br>
2023-02-08 23:44:29 -0300

    Iniciado JavaScript do projeto

---
+++ js/controllers/form-controller.js
@@ -0,0 +1,79 @@
+import Address from'../models/address.js';
+
+function State() {
+    this.address = new Address();
+
+    this.btnSave = null;
+    this.btnClear = null;
+
+    this.inputCep = null;
+    this.inputStreet = null;
+    this.inputNumber = null;
+    this.inputCity = null;
+
+    this.errorCep = null;
+    this.erroNumber = null;
+}
+
+const state = new State();
+
+export function init() {
+
+state.inputCep = document.forms.newAddress.cep;
+state.inputStreet = document.forms.newAddress.street;
+state.inputNumber = document.forms.newAddress.number;
+state.inputCity = document.forms.newAddress.city;
+
+state.btnSave = document.forms.newAddress.btnSave;
+state.btnClear = document.forms.newAddress.btnClear;
+
+state.errorCep = document.querySelector('[data-error="cep"]');
+state.errorNumber = document.querySelector('[data-error="number"]');
+
+state.inputNumber.addEventListener('change', handleInputNumberChange);
+state.inputCep.addEventListener('change', handleInputCepChange);
+state.btnClear.addEventListener('click', handleBtnClearClick);
+}
+
+function handleInputNumberChange(event) {
+    if (event.target.value == "") {
+        setFormError("number", "Campo requerido");
+    }
+    else {
+        setFormError("number", "");
+    }
+}
+
+function handleInputCepChange(event) {
+    if (event.target.value == "") {
+        setFormError("cep", "Campo requerido");
+    }
+    else {
+        setFormError("cep", "");
+    }
+}
+
+function handleBtnClearClick(event) {
+    event.preventDefault();
+    clearForm();
+}
+
+function clearForm(){
+
+    state.inputCep.value = "";
+    state.inputCity.value = "";
+    state.inputNumber.value = "";
+    state.inputStreet.value = "";
+
+    setFormError("cep", "");
+    setFormError("number", "");
+
+    state.inputCep.focus();
+}
+
+
+function setFormError(key, value) {
+    const element = document.querySelector(`[data-error="${key}"]`);
+    element.innerHTML = value;
+
+}
