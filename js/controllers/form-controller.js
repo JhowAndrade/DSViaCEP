898415ba773497952ec8f18cce7aba900f866912
Johanny <johanny_andrade@yahoo.com.br>
2023-02-16 23:56:32 -0300

    Terminado projeto DSViaCEP

--- js/controllers/form-controller.js
+++ js/controllers/form-controller.js
@@ -72,9 +72,20 @@ async function handleInputCepChange(event) {
 
 }
 
-async function handleBtnSaveClick(event) {
+function handleBtnSaveClick(event) {
     event.preventDefault();
-    listController.addCard(state.address);
+
+    const errors = addressService.getErrors(state.address);
+    const keys = Object.keys(errors);
+
+    if (keys.length > 0) {
+       keys.forEach(key => {
+        setFormError(key, errors[key]);
+       });
+    } else {
+        listController.addCard(state.address);
+        clearForm();
+    }
 }
 
 function handleInputNumberChange(event) {
@@ -100,6 +111,8 @@ function clearForm() {
 
     setFormError("cep", "");
     setFormError("number", "");
+
+    state.address = new Address();
 
     state.inputCep.focus();
 }
